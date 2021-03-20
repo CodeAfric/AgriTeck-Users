172cc0484217425b2434d46aab4872851fdbe4c3
David-Blessie <78177066+David-Blessie@users.noreply.github.com>
2021-03-19 17:43:17 -0700

    The frames

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:agriteck_user/Home_Page/HomeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:agriteck_user/Home_Page/HomeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,25 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: diseases_HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+/* class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -110,4 +104,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+} */
